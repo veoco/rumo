8cf0f77bb03fa437c732ed0d20d87c23ea514d8d
veoco <one@nomox.cn>
2022-12-10 16:34:47 +0800

    新增对 get_user_by_id 和 modify_user_by_id 的测试

--- tests/test_users.rs
+++ tests/test_users.rs
@@ -1,9 +1,10 @@
 use axum::{
-    body::Body,
+    body::{Body},
     http::{self, Request, StatusCode},
 };
-use serde_json::json;
+use serde_json::{json, Value};
 use tower::ServiceExt;
+use hyper::body::to_bytes;
 
 mod common;
 use common::{setup_app, setup_state};
@@ -43,6 +44,7 @@ async fn login_success() {
     let state = setup_state().await;
     let app = setup_app(state.clone()).await;
 
+    // register a normal user
     let data = json!({"name": "login_test","mail": "login_success@test.local", "url": "http://127.0.0.1", "password": "password"}).to_string();
     let request = Request::builder()
         .method(http::Method::POST)
@@ -66,3 +68,74 @@ async fn login_success() {
 
     assert_eq!(response.status(), StatusCode::OK);
 }
+
+#[tokio::test]
+async fn normal_user_change_success() {
+    let state = setup_state().await;
+    let app = setup_app(state.clone()).await;
+
+    // register a normal user
+    let data = json!({"name": "change_test","mail": "change_test@test.local", "url": "http://127.0.0.1", "password": "password"}).to_string();
+    let request = Request::builder()
+        .method(http::Method::POST)
+        .uri("/api/users")
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .body(Body::from(data))
+        .unwrap();
+    let response = app.oneshot(request).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+    let body = to_bytes(response.into_body()).await.unwrap();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    let uid = body.get("id");
+    assert!(uid.is_some());
+    let uid = uid.unwrap().as_u64().unwrap();
+
+    let app = setup_app(state.clone()).await;
+    let data = json!({"mail": "change_test@test.local", "password": "password"}).to_string();
+    let request = Request::builder()
+        .method(http::Method::POST)
+        .uri("/api/users/token")
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .body(Body::from(data))
+        .unwrap();
+    let response = app.oneshot(request).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = to_bytes(response.into_body()).await.unwrap();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    let token = body.get("access_token");
+    assert!(token.is_some());
+
+    let token = token.unwrap().as_str().unwrap();
+    let app = setup_app(state.clone()).await;
+    let data = json!({"name": "changed_test", "mail": "changed_test@test.local", "url": "http://127.0.0.1", "screenName": "changed_test", "group": "subscriber"}).to_string();
+    let request = Request::builder()
+        .method(http::Method::PATCH)
+        .uri(format!("/api/users/{}", uid))
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .header(http::header::AUTHORIZATION, format!("Bearer {}", token))
+        .body(Body::from(data))
+        .unwrap();
+    let response = app.oneshot(request).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let app = setup_app(state.clone()).await;
+    let request = Request::builder()
+        .method(http::Method::GET)
+        .uri(format!("/api/users/{}", uid))
+        .header(http::header::CONTENT_TYPE, "application/json")
+        .header(http::header::AUTHORIZATION, format!("Bearer {}", token))
+        .body(Body::empty())
+        .unwrap();
+    let response = app.oneshot(request).await.unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = to_bytes(response.into_body()).await.unwrap();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    let name = body.get("name");
+    assert!(name.is_some());
+
+    let name = name.unwrap().as_str().unwrap();
+    assert_eq!(name, "changed_test");
+}
+
