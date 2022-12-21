7875400e7423af7bcc02173f32f8e4be70c08714
veoco <one@nomox.cn>
2022-12-21 20:23:49 +0800

    新增标签下文章 api 及测试

--- src/tags/urls.rs
+++ src/tags/urls.rs
@@ -10,6 +10,7 @@ use crate::AppState;
 pub fn tags_routers() -> Router<Arc<AppState>> {
     let tags_route = Router::new()
         .route("/api/tags/", get(views::list_tags).post(views::create_tag))
-        .route("/api/tags/:slug", get(views::get_tag_by_slug));
+        .route("/api/tags/:slug", get(views::get_tag_by_slug))
+        .route("/api/tags/:slug/posts/", post(views::add_post_to_tag).get(views::list_tag_posts_by_slug));;
     tags_route
 }
--- tests/test_tags.rs
+++ tests/test_tags.rs
@@ -33,3 +33,41 @@ async fn create_then_get_tag_by_slug_success() {
     let (status_code, _) = get("/api/tags/test-tag-create").await;
     assert_eq!(status_code, StatusCode::OK);
 }
+
+#[tokio::test]
+async fn create_then_list_tag_posts_success() {
+    let data = json!({"name": "testTagPost", "slug": "test-tag-post"}).to_string();
+    let (status_code, _) = admin_post("/api/tags/", data).await;
+    assert_eq!(status_code, StatusCode::OK);
+
+    let (status_code, body) = get("/api/tags/test-tag-post/posts/?page=1&page_size=10&order_by=-cid").await;
+    assert_eq!(status_code, StatusCode::OK);
+
+    let body = body.unwrap();
+    let count = body.get("count").unwrap().as_u64().unwrap();
+
+    let data = json!({
+        "title": "testPostTag",
+        "slug": "test-post-tag",
+        "created": 1666666666,
+        "text": "testText",
+        "status": "publish",
+        "allowComment": "1",
+        "allowPing": "1",
+        "allowFeed": "1",
+    })
+    .to_string();
+    let (status_code, _) = admin_post("/api/posts/", data).await;
+    assert_eq!(status_code, StatusCode::OK);
+
+    let data = json!({"slug": "test-post-tag",}).to_string();
+    let (status_code, _) = admin_post("/api/tags/test-tag-post/posts/", data).await;
+    assert_eq!(status_code, StatusCode::OK);
+
+    let (status_code, body) = get("/api/tags/test-tag-post/posts/?page=1&page_size=10&order_by=-cid").await;
+    assert_eq!(status_code, StatusCode::OK);
+
+    let body = body.unwrap();
+    let new_count = body.get("count").unwrap().as_u64().unwrap();
+    assert!(new_count > count);
+}
