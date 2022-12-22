f39b9da074115a83419f4d3dfa04d4efba533535
veoco <one@nomox.cn>
2022-12-22 16:33:03 +0800

    调整 category 测试

--- tests/test_categories.rs
+++ tests/test_categories.rs
@@ -14,7 +14,7 @@ async fn create_then_list_categories_success() {
 
     let data = json!({"name": "testCategory", "slug": "test-category"}).to_string();
     let (status_code, _) = admin_post("/api/categories/", data).await;
-    assert_eq!(status_code, StatusCode::OK);
+    assert_eq!(status_code, StatusCode::CREATED);
 
     let (status_code, body) = get("/api/categories/?page=1&page_size=10&order_by=-mid").await;
     assert_eq!(status_code, StatusCode::OK);
@@ -28,7 +28,7 @@ async fn create_then_list_categories_success() {
 async fn create_then_get_category_by_slug_success() {
     let data = json!({"name": "testCategoryCreate", "slug": "test-category-create"}).to_string();
     let (status_code, _) = admin_post("/api/categories/", data).await;
-    assert_eq!(status_code, StatusCode::OK);
+    assert_eq!(status_code, StatusCode::CREATED);
 
     let (status_code, _) = get("/api/categories/test-category-create").await;
     assert_eq!(status_code, StatusCode::OK);
@@ -38,7 +38,7 @@ async fn create_then_get_category_by_slug_success() {
 async fn create_then_list_category_posts_success() {
     let data = json!({"name": "testCategoryPost", "slug": "test-category-post"}).to_string();
     let (status_code, _) = admin_post("/api/categories/", data).await;
-    assert_eq!(status_code, StatusCode::OK);
+    assert_eq!(status_code, StatusCode::CREATED);
 
     let (status_code, body) = get("/api/categories/test-category-post/posts/?page=1&page_size=10&order_by=-cid").await;
     assert_eq!(status_code, StatusCode::OK);
@@ -58,11 +58,11 @@ async fn create_then_list_category_posts_success() {
     })
     .to_string();
     let (status_code, _) = admin_post("/api/posts/", data).await;
-    assert_eq!(status_code, StatusCode::OK);
+    assert_eq!(status_code, StatusCode::CREATED);
 
     let data = json!({"slug": "test-post-category",}).to_string();
     let (status_code, _) = admin_post("/api/categories/test-category-post/posts/", data).await;
-    assert_eq!(status_code, StatusCode::OK);
+    assert_eq!(status_code, StatusCode::CREATED);
 
     let (status_code, body) = get("/api/categories/test-category-post/posts/?page=1&page_size=10&order_by=-cid").await;
     assert_eq!(status_code, StatusCode::OK);
