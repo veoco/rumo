22f7466ed5ace47707868f992113d2b570024592
veoco <one@nomox.cn>
2023-01-01 17:01:39 +0800

    新增附件 text 序列化与反序列化

---
+++ src/attachments/de.rs
@@ -0,0 +1,377 @@
+use std::ops::{AddAssign, MulAssign};
+
+use serde::de::{self, DeserializeSeed, MapAccess, Visitor};
+use serde::Deserialize;
+
+use super::errors::{Error, Result};
+
+pub struct Deserializer<'de> {
+    input: &'de str,
+}
+
+impl<'de> Deserializer<'de> {
+    pub fn from_str(input: &'de str) -> Self {
+        Deserializer { input }
+    }
+}
+
+pub fn from_str<'a, T>(s: &'a str) -> Result<T>
+where
+    T: Deserialize<'a>,
+{
+    let mut deserializer = Deserializer::from_str(s);
+    let t = T::deserialize(&mut deserializer)?;
+    if deserializer.input.is_empty() {
+        Ok(t)
+    } else {
+        Err(Error::TrailingCharacters)
+    }
+}
+
+impl<'de> Deserializer<'de> {
+    fn peek_char(&mut self) -> Result<char> {
+        self.input.chars().next().ok_or(Error::Eof)
+    }
+
+    fn next_char(&mut self) -> Result<char> {
+        let ch = self.peek_char()?;
+        self.input = &self.input[ch.len_utf8()..];
+        Ok(ch)
+    }
+
+    fn parse_unsigned<T>(&mut self) -> Result<T>
+    where
+        T: AddAssign<T> + MulAssign<T> + From<u8>,
+    {
+        if self.next_char()? != 'i' {
+            return Err(Error::ExpectedString);
+        }
+        self.next_char()?;
+        let mut int = match self.next_char()? {
+            ch @ '0'..='9' => T::from(ch as u8 - b'0'),
+            _ => {
+                return Err(Error::ExpectedInteger);
+            }
+        };
+        loop {
+            match self.input.chars().next() {
+                Some(ch @ '0'..='9') => {
+                    self.input = &self.input[1..];
+                    int *= T::from(10);
+                    int += T::from(ch as u8 - b'0');
+                }
+                _ => {
+                    self.next_char()?;
+                    return Ok(int);
+                }
+            }
+        }
+    }
+
+    fn parse_string(&mut self) -> Result<&'de str> {
+        if self.next_char()? != 's' {
+            return Err(Error::ExpectedString);
+        }
+
+        let left = self.input.find('"').unwrap_or(0);
+        self.input = &self.input[left+1..];
+
+        match self.input.find('"') {
+            Some(len) => {
+                let s = &self.input[..len];
+                self.input = &self.input[len + 2..];
+                Ok(s)
+            }
+            None => Err(Error::Eof),
+        }
+    }
+}
+
+impl<'de, 'a> de::Deserializer<'de> for &'a mut Deserializer<'de> {
+    type Error = Error;
+
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        match self.peek_char()? {
+            's' => self.deserialize_str(visitor),
+            'i' => self.deserialize_u64(visitor),
+            'a' => self.deserialize_map(visitor),
+            _ => Err(Error::Syntax),
+        }
+    }
+
+    fn deserialize_bool<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_i8<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_i16<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_i32<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_i64<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u8<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u16<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u32<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        visitor.visit_u64(self.parse_unsigned()?)
+    }
+
+    fn deserialize_f32<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_f64<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_char<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        visitor.visit_borrowed_str(self.parse_string()?)
+    }
+
+    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_str(visitor)
+    }
+
+    fn deserialize_bytes<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_byte_buf<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_option<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_unit<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_unit_struct<V>(self, _name: &'static str, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_newtype_struct<V>(self, _name: &'static str, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_seq<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_tuple<V>(self, _len: usize, _visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_tuple_struct<V>(
+        self,
+        _name: &'static str,
+        _len: usize,
+        _visitor: V,
+    ) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_map<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        if self.next_char()? == 'a' {
+            let left = self.input.find('{').unwrap_or(0);
+            self.input = &self.input[left+1..];
+            
+            let value = visitor.visit_map(SemicolonSeparated { de: self })?;
+            if self.next_char()? == '}' {
+                Ok(value)
+            } else {
+                Err(Error::ExpectedMapEnd)
+            }
+        } else {
+            Err(Error::ExpectedMap)
+        }
+    }
+
+    fn deserialize_struct<V>(
+        self,
+        _name: &'static str,
+        _fields: &'static [&'static str],
+        visitor: V,
+    ) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_map(visitor)
+    }
+
+    fn deserialize_enum<V>(
+        self,
+        _name: &'static str,
+        _variants: &'static [&'static str],
+        _visitor: V,
+    ) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        unimplemented!()
+    }
+
+    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_str(visitor)
+    }
+
+    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        self.deserialize_any(visitor)
+    }
+}
+
+struct SemicolonSeparated<'a, 'de: 'a> {
+    de: &'a mut Deserializer<'de>,
+}
+
+impl<'de, 'a> MapAccess<'de> for SemicolonSeparated<'a, 'de> {
+    type Error = Error;
+
+    fn next_key_seed<K>(&mut self, seed: K) -> Result<Option<K::Value>>
+    where
+        K: DeserializeSeed<'de>,
+    {
+        if self.de.peek_char()? == '}' {
+            return Ok(None);
+        }
+        seed.deserialize(&mut *self.de).map(Some)
+    }
+
+    fn next_value_seed<V>(&mut self, seed: V) -> Result<V::Value>
+    where
+        V: DeserializeSeed<'de>,
+    {
+        seed.deserialize(&mut *self.de)
+    }
+}
+
+#[test]
+fn test_from_string() {
+    use super::models::AttachmentText;
+
+    let j = r#"a:5:{s:4:"name";s:17:"mate10-emui-1.png";s:4:"path";s:35:"/usr/uploads/2022/12/3448342241.png";s:4:"size";i:14660;s:4:"type";s:3:"png";s:4:"mime";s:9:"image/png";}"#;
+    let expected = AttachmentText {
+        name: "mate10-emui-1.png".to_string(),
+        path: "/usr/uploads/2022/12/3448342241.png".to_string(),
+        size: 14660,
+        r#type: "png".to_string(),
+        mime: "image/png".to_string(),
+    };
+    assert_eq!(expected, from_str(j).unwrap());
+
+    let j = r#"a:5:{s:4:"name";s:22:"https-get-status-1.png";s:4:"path";s:35:"/usr/uploads/2022/12/2415673155.png";s:4:"size";i:25523;s:4:"type";s:3:"png";s:4:"mime";s:9:"image/png";}"#;
+    let expected = AttachmentText {
+        name: "https-get-status-1.png".to_string(),
+        path: "/usr/uploads/2022/12/2415673155.png".to_string(),
+        size: 25523,
+        r#type: "png".to_string(),
+        mime: "image/png".to_string(),
+    };
+    assert_eq!(expected, from_str(j).unwrap());
+}
---
+++ src/attachments/ser.rs
@@ -0,0 +1,334 @@
+use serde::{ser, Serialize};
+
+use super::errors::{Error, Result};
+
+pub struct Serializer {
+    output: String,
+}
+
+pub fn to_string<T>(value: &T) -> Result<String>
+where
+    T: Serialize,
+{
+    let mut serializer = Serializer {
+        output: String::new(),
+    };
+    value.serialize(&mut serializer)?;
+    Ok(serializer.output)
+}
+
+impl<'a> ser::Serializer for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    type SerializeSeq = Self;
+    type SerializeTuple = Self;
+    type SerializeTupleStruct = Self;
+    type SerializeTupleVariant = Self;
+    type SerializeMap = Self;
+    type SerializeStruct = Self;
+    type SerializeStructVariant = Self;
+
+    fn serialize_bool(self, _v: bool) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_i8(self, _v: i8) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_i16(self, _v: i16) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_i32(self, _v: i32) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_u8(self, _v: u8) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_u16(self, _v: u16) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_u32(self, _v: u32) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_u64(self, v: u64) -> Result<()> {
+        self.output += &(String::from("i:") + &v.to_string());
+        Ok(())
+    }
+
+    fn serialize_f32(self, _v: f32) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_f64(self, _v: f64) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_char(self, _v: char) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_str(self, v: &str) -> Result<()> {
+        self.output += &format!("s:{}:\"", v.len());
+        self.output += v;
+        self.output += "\"";
+        Ok(())
+    }
+
+    fn serialize_bytes(self, _v: &[u8]) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_none(self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_some<T>(self, _v: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn serialize_unit(self) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+    ) -> Result<()> {
+        unimplemented!()
+    }
+
+    fn serialize_newtype_struct<T>(self, _name: &'static str, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn serialize_newtype_variant<T>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _value: &T,
+    ) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq> {
+        unimplemented!()
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple> {
+        unimplemented!()
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleStruct> {
+        unimplemented!()
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleVariant> {
+        unimplemented!()
+    }
+
+    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap> {
+        self.output += &format!("a:{}:{{", len.unwrap_or(0));
+        Ok(self)
+    }
+
+    fn serialize_struct(self, _name: &'static str, len: usize) -> Result<Self::SerializeStruct> {
+        self.serialize_map(Some(len))
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeSeq for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeTuple for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_element<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeTupleStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeTupleVariant for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeMap for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_key<T>(&mut self, _key: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn serialize_value<T>(&mut self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+impl<'a> ser::SerializeStruct for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, key: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        if !self.output.ends_with('{') {
+            self.output += ";";
+        }
+        key.serialize(&mut **self)?;
+        self.output += ";";
+        value.serialize(&mut **self)
+    }
+
+    fn end(self) -> Result<()> {
+        self.output += ";}";
+        Ok(())
+    }
+}
+
+impl<'a> ser::SerializeStructVariant for &'a mut Serializer {
+    type Ok = ();
+    type Error = Error;
+
+    fn serialize_field<T>(&mut self, _key: &'static str, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        unimplemented!()
+    }
+
+    fn end(self) -> Result<()> {
+        unimplemented!()
+    }
+}
+
+#[test]
+fn test_to_string() {
+    use super::models::AttachmentText;
+
+    let s = AttachmentText {
+        name: "mate10-emui-1.png".to_string(),
+        path: "/usr/uploads/2022/12/3448342241.png".to_string(),
+        size: 14660,
+        r#type: "png".to_string(),
+        mime: "image/png".to_string(),
+    };
+    let expected = r#"a:5:{s:4:"name";s:17:"mate10-emui-1.png";s:4:"path";s:35:"/usr/uploads/2022/12/3448342241.png";s:4:"size";i:14660;s:4:"type";s:3:"png";s:4:"mime";s:9:"image/png";}"#;
+    assert_eq!(to_string(&s).unwrap(), expected);
+
+    let s = AttachmentText {
+        name: "https-get-status-1.png".to_string(),
+        path: "/usr/uploads/2022/12/2415673155.png".to_string(),
+        size: 25523,
+        r#type: "png".to_string(),
+        mime: "image/png".to_string(),
+    };
+    let expected = r#"a:5:{s:4:"name";s:22:"https-get-status-1.png";s:4:"path";s:35:"/usr/uploads/2022/12/2415673155.png";s:4:"size";i:25523;s:4:"type";s:3:"png";s:4:"mime";s:9:"image/png";}"#;
+    assert_eq!(to_string(&s).unwrap(), expected);
+}
